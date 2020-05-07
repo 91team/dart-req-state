7a50ed2b3d66c4cf4d5f7486be2a0feba507041e
es <es@MacBook-Pro-mac.local>
2020-05-07 23:27:46 +0300

    Added Condition widget

--- lib/dart_req_state.dart
+++ lib/dart_req_state.dart
@@ -2,4 +2,5 @@ library req_state;
 
 export 'src/req_state.dart';
 export 'src/req_state_statuses.dart';
+export 'src/widgets/condition.dart';
 export 'src/widgets/switcher.dart';
---
+++ lib/src/widgets/condition.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/widgets.dart';
+
+import '../req_state.dart';
+import '../req_state_statuses.dart';
+
+typedef _THandler = Widget Function(
+  BuildContext context, {
+  @required ReqStateStatus status,
+});
+
+class ReqStateCondition<TIF> extends StatelessWidget {
+  final Widget _defaultWidget;
+  final ReqState _reqState;
+
+  final _THandler onIf;
+  final _THandler onElse;
+
+  const ReqStateCondition(
+    ReqState reqState, {
+    Key key,
+    this.onIf,
+    this.onElse,
+  })  : _defaultWidget = const SizedBox.shrink(),
+        _reqState = reqState,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<ReqStateStatus>(
+      stream: _reqState.stream,
+      initialData: _reqState.status,
+      builder: (
+        BuildContext streamBuilderContext,
+        AsyncSnapshot<ReqStateStatus> snapshot,
+      ) {
+        if (snapshot.data is TIF) {
+          return _execHandler(
+            context: context,
+            listener: onIf,
+          );
+        }
+
+        if (onElse != null) {
+          return _execHandler(
+            context: context,
+            listener: onIf,
+          );
+        }
+
+        return _defaultWidget;
+      },
+    );
+  }
+
+  Widget _execHandler({
+    BuildContext context,
+    _THandler listener,
+  }) {
+    return listener != null ? listener(context, status: _reqState.status) : _defaultWidget;
+  }
+}
